a1be642cb0c56c1c08c7e7fb15eca9cad17e16c5
Carlos de la Guardia <cguardia@yahoo.com>
2019-07-23 19:54:25 -0500

    try to cleanup before

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -52,7 +52,7 @@ export PROJECT_ID=$(cat "${KOKORO_GFILE_DIR}/project-id.json")
 # cd "$PACKAGE"
 
 # Some system tests require indexes. Use gclod to create to create them.
-gcloud info --run-diagnostics
+gcloud --quiet datastore indexes cleanup tests/system/index.yaml
 gcloud --quiet datastore indexes create tests/system/index.yaml
 
 # Remove old nox
