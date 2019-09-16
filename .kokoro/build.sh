bd0cc1411a44dec33a83fb3d81ccaf76b1dcba08
Chris Wilcox <crwilcox@google.com>
2019-09-16 10:44:05 -0700

    start redis-server

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -42,7 +42,7 @@ export PROJECT_ID=$(cat "${KOKORO_GFILE_DIR}/project-id.json")
 
 # Configure Local Redis to be used
 export REDIS_CACHE_URL=redis://localhost
-
+redis-server &
 
 # Some system tests require indexes. Use gcloud to create them.
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS --project=$PROJECT_ID
