8fa26268ede41f3ba8da79656d8feb713e16919c
Christopher Wilcox <crwilcox@google.com>
2019-11-06 12:57:49 -0800

    fix: Add python 2 as a trove classifier to setup

--- setup.py
+++ setup.py
@@ -50,6 +50,8 @@ def main():
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
             "Operating System :: OS Independent",
             "Topic :: Internet",
         ],
