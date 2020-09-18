41ff895479e3a598b5385922ee76f0508d4b3dee
Chris Rossi <chris@archimedeanco.com>
2020-09-18 10:28:01 -0400

    Be explicit about ipv4 vs ipv6

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -39,7 +39,7 @@ export REDIS_CACHE_URL=redis://localhost
 redis-server &
 
 # Configure local memcached to be used
-export MEMCACHED_HOSTS=localhost
+export MEMCACHED_HOSTS=127.0.0.1
 echo "Starting memcached..."
 cat /etc/memcached.conf
 service memcached start
