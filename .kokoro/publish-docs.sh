c05b4ca122ac4950a489c8fdc24c4536a9078b8d
Chris Wilcox <crwilcox@google.com>
2020-09-21 11:02:45 -0700

    docs: add enchant and other deps to docs publish

--- .kokoro/publish-docs.sh
+++ .kokoro/publish-docs.sh
@@ -20,6 +20,11 @@ export PYTHONUNBUFFERED=1
 
 export PATH="${HOME}/.local/bin:${PATH}"
 
+# Need enchant for spell check
+sudo apt-get update
+sudo apt-get -y install dictionaries-common aspell aspell-en \
+                        hunspell-en-us libenchant1c2a enchant binutils
+
 # Install nox
 python3 -m pip install --user --upgrade --quiet nox
 python3 -m nox --version
