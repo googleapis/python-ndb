98c8ac114f9126bc72c00bd22b1a29f3c4860b02
Chris Wilcox <crwilcox@google.com>
2021-01-22 09:57:15 -0800

    fix: Detect if bytes encoding needed when loading from pickled data

--- google/cloud/ndb/model.py
+++ google/cloud/ndb/model.py
@@ -2920,6 +2920,8 @@ class PickleProperty(BlobProperty):
         Returns:
             Any: The unpickled ``value``.
         """
+        if type(value) is bytes:
+            return pickle.loads(value, encoding="bytes")
         return pickle.loads(value)
 
 
